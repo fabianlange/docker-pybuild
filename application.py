6c3c476cfaed3b7fb0d445fbb1f16ad93804055f
Fabian Lange <fabian.lange@srccast.de>
2025-06-25 09:29:38 +0200

    Add example application

---
+++ application.py
@@ -0,0 +1,23 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "requests<3"
+# ]
+# [tool.docker]
+# Dockerfile = """
+#   FROM python:3.11
+#   RUN pip install pipx
+#   WORKDIR /app
+#   COPY application.py /app
+#   EXPOSE 8080
+#   ENTRYPOINT ["pipx", "run", "/app/application.py"]
+# """
+# ///
+
+import sys
+
+import requests
+
+project = sys.argv[1]
+pipx_data = requests.get(f"https://pypi.org/pypi/{project}/json").json()
+print(pipx_data["info"]["version"])
